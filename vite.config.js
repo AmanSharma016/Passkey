2fe1c3b1e14abdf2dc0224854c52c95efb361a33
Aman Sharma <asamansharma016@gmail.com>
2025-02-18 00:10:42 +0530

    Added allowedHosts for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',  // Bind to all network interfaces
     port: process.env.PORT || 5173,  // Use Render's assigned port or default to 5173
-  }
+    allowedHosts: [
+      'passkey-4yw1.onrender.com', // Add this host to the allowed hosts
+      'localhost',
+      '127.0.0.1',
+    ],
+  },
 });
